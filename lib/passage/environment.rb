37cbac6be40e11f6b19662da23a516e742df4e9d
Dotan Nahum <jondotan@gmail.com>
2011-03-19 14:15:55 +0200

    version bump 0.2.0

--- lib/passage/environment.rb
+++ lib/passage/environment.rb
@@ -9,15 +9,25 @@ module Passage
 
       set :sessions, true
       set :log, opts[:logger] || Logger.new(STDOUT)
-      set :identities, opts[:ids_file].nil? ? {} : YAML::load_file(opts[:ids_file])
 
+      # load identities
+      if(opts[:ids_file])
+        ids = YAML::load_file(opts[:ids_file])
+        set :identities, ids
+        log.info "loaded #{ids.keys.count} identities from #{opts[:ids_file]}"
+      else
+        set :identities, {}
+        log.info "no identities loaded (free for all)"
+      end
+
+      # load auth
       Dir.glob(File.expand_path("auth/**/*.rb", File.dirname(__FILE__))).each do |f|
         require f
         log.debug "discovered auth: #{File.basename(f)[0..-4]}"
       end
-
       register constantize(opts[:auth])
       log.info "loaded auth: #{opts[:auth]}"
+
     end
 
     def constantize(word)
--- passage.gemspec
+++ passage.gemspec
@@ -9,7 +9,7 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Dotan Nahum"]
-  s.date = %q{2011-03-13}
+  s.date = %q{2011-03-14}
   s.default_executable = %q{passage}
   s.description = %q{A simple, tweakable OpenID provider for private use.}
   s.email = %q{jondotan@gmail.com}
